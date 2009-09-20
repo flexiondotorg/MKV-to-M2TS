4a80b86b1054888069ad355ee506cbd01d302ce3
Martin Wimpress <code@flexion.org>
2009-09-20 18:34:31 +0100

    Update build script

--- build.sh
+++ build.sh
@@ -2,7 +2,9 @@
 
 function build {
     RELEASE_NAME="MKV-to-M2TS"
-    RELEASE_VER=1.1
+    RELEASE_VER="1.1"
+    RELEASE_KEYWORDS="MKV, M2TS, conversion, PS3, PlayStation 3, Linux, script"
+    RELEASE_DESC="Creates a PlayStation 3 compatible MPEG-2 Transport Stream from a Matroska"
 
     rm ${RELEASE_NAME}-v${RELEASE_VER}.tar* 2>/dev/null
     bzr export ${RELEASE_NAME}-v${RELEASE_VER}.tar
